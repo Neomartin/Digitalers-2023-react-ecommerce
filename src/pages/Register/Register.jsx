390ae753c77a97b725dfcfa145f964990cc446ab
Martin Rodriguez <martin.rodriguez@naranja.com>
2023-11-07 19:07:51 -0300

    React form register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Register = () => {
+  const { register, handleSubmit } = useForm()
+
+  function formSubmited(data) {
+    console.log(data)
+  }
+
   return (
-    <h1>Register</h1>
+    <>
+      <form onSubmit={handleSubmit(formSubmited)}>
+        <input {...register("fullname", { type:"text"})  }  />
+        <input {...register("email", { type:"email"})  }  />
+        <input {...register("password", { type:"password"})  }  />
+        <input {...register("avatar", { type:"url"})  }  />
+
+        <button type='submit'>Registrar</button>
+
+      </form>
+    
+    
+    </>
   )
 }
 
